f2aaee6218f7ecafe42b662c1b70663569511bda
Vidurshika <165041842+Vidurshika@users.noreply.github.com>
2025-05-02 00:00:10 +0530

    completed sign up

---
+++ frontend/finance-tracker/src/components/input/ProfilePhotoSelector.jsx
@@ -0,0 +1,79 @@
+import React, { useRef, useState } from 'react';
+/* useRef is a special React hook that lets you point to an element in your HTML, like a file input or a button. */
+/* import { LuUser, LuUpload, LuTrash } from "react-icons/lu"; fa or lu*/
+import { FaUserAlt , FaTrashAlt} from "react-icons/fa";
+import { FiUpload } from "react-icons/fi";
+
+const ProfilePhotoSelector = ({image,setImage}) => {
+    const inputRef = useRef(null);
+    const [previewUrl,setPreviewUrl] = useState(null); 
+    
+    const handleImageChange = (event) =>{
+        const file = event.target.files[0];
+        if(file){
+            setImage(file);
+            const preview = URL.createObjectURL(file); //Make a quick preview link from the image file, and store it in previewUrl so I can show it in the UI.
+            setPreviewUrl(preview);
+        }
+    };
+    /* tempory url
+    That image is a file stored in your computer — it's not a link or URL yet.
+But you want to show a preview of that image inside your app before uploading it anywhere,So, React (JavaScript) uses this:
+URL.createObjectURL(file).It turns your file into a temporary link that looks like:blob:http://localhost:3000/2dfg3f4-abc...
+This is called a temporary preview URL — because:It disappears when you reload the page or remove the image.*/
+
+    const handleRemoveImage = () => {
+        setImage(null);
+        setPreviewUrl(null);
+    };
+
+    const onChooseFile = () => {
+        inputRef.current.click();
+    };
+
+    /* const inputRef = useRef(null);You’re creating a reference named inputRef.
+At first, it’s empty (null).Then, you connect it to your <input> like this:
+<input ref={inputRef} ... />Now, React knows:inputRef.current means this <input> element.
+🔹 Why use it? Because the input is hidden (className="hidden"), the user can’t click it directly.
+So you do this:const onChooseFile = () => {
+  inputRef.current.click();  // 👈 This simulates a user click on the input
+};Even though the input is invisible, this line will open the file chooser.*/
+
+  return (
+    <div className='flex justify-center mb-6'>
+        <input   //Browsers automatically show a file picker with "Choose File" 
+            type="file" 
+            accept='image/*'
+            ref={inputRef}
+            onChange={handleImageChange}
+            className='hidden'
+        />
+        {!image ? (
+            <div className='w-20 h-20 flex items-center justify-center bg-blue-100 rounded-full relative '>
+               <FaUserAlt className="text-4xl text-blue-400"/>
+                <button
+                 type='button'
+                 className='w-8 h-8 flex items-center justify-center bg-blue-400 text-white rounded-full absolute -bottom-1 -right-1 hover:bg-blue-600 transition'
+                 onClick={onChooseFile}
+                >
+                    <FiUpload/>
+                </button> 
+
+            </div>
+        ):(
+            <div className='relative'>
+                <img src={previewUrl} alt="prof pic" className='w-20 h-20 rounded-full object-cover' />
+                <button 
+                    type='button' 
+                    className='w-8 h-8 flex items-center justify-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1 hover:bg-red-700 transition'
+                    onClick={handleRemoveImage}
+                >
+                    <FaTrashAlt/>
+                </button>
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default ProfilePhotoSelector
--- frontend/finance-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/finance-tracker/src/pages/Auth/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
+import ProfilePhotoSelector from '../../components/input/ProfilePhotoSelector'
 import {useNavigate,Link } from 'react-router-dom'
 import Input from '../../components/input/Input'
 import { validateEmail } from '../../utils/helper';
@@ -14,11 +15,85 @@ const SignUp = () => {
   const [error,setError] = useState(null);
   const navigate = useNavigate();
 
+  const handleSignUp = async (e) =>{
+    e.preventDefault();
+    let profileImageUrl ="";
+
+    if(!fullName) {
+      setError("Please enter your name");
+      return;
+    }
+    if(!validateEmail(email)) {
+      setError("Please enter a valid email");
+      return;
+    }
+    if(!password) {
+      setError("Please enter your password");
+      return;
+    }
+
+    setError("");
+
+    /* sign up api call */
+  }
+
   return (
     <AuthLayout>
+      <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-o flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
+        <p className='text-xs font-semibold text-blue-700 mt-[5px] mb-6'>
+          Join us today by entering your details below
+        </p>
 
+        <form onSubmit={handleSignUp}>
+
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
+
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+            <Input 
+              type="text"
+              value={fullName}
+              onChange={({target})=> setFullName(target.value)}
+              label="Full Name"
+              placeholder="Enter name"
+            />
+
+            <Input 
+              value={email}
+              onChange={({target}) => setEmail(target.value)}
+              label="Email Address"
+              placeholder='Enter email address'
+              type='text' 
+            />
+            <div className='md:col-span-2'> {/* like merging 2 cols */}
+              <Input 
+                value={password}
+                onChange={({target}) => setPassword(target.value)}
+                label="Password"
+                placeholder='Enter password'
+                type='password' 
+              />
+            </div>
+
+          </div>
+
+          {error && <p className='text-red-500 text-xs pb-2.5 font-semibold'>{error}</p>}
+          
+          <button className='btn-primary' type='submit'>
+            Sign Up
+          </button>
+
+          <p className='text-[13px] text-black mt-3'>
+            Already have an account? 😮{" "}
+            <Link className="font-medium text-blue-800 underline" to="/login">
+              Login
+            </Link>
+          </p>
+
+        </form>
+      </div>
     </AuthLayout>
   )
 }
 
-export default SignUp
+export default SignUp;
