315a152f3befa42e96db206a506cd91491a07268
Vidurshika <165041842+Vidurshika@users.noreply.github.com>
2025-06-30 01:15:28 +0530

    helper functions for CreateAvatar and card values

--- frontend/finance-tracker/src/utils/helper.js
+++ frontend/finance-tracker/src/utils/helper.js
@@ -7,3 +7,24 @@ export const validateEmail = (email) =>{
 Takes an email string as input.
 Uses a regular expression (regex) to check if it's in the proper email format.
 Returns true if it's valid, false otherwise. */
+
+export const getInitials = (name) => {
+    if(!name) return "";
+    const words = name.split(" ");
+    let initials ="";
+    for (let i = 0; i<Math.min(words.length, 2); i++) {
+        initials += words[i][0];
+    }
+    return initials.toUpperCase();//Converts the final initials to uppercase before returning
+};
+
+export const addThousandsSeparator = (num) => {
+    if (num == null || isNaN(num)) return "";
+
+    const [integerPart, fractionPart] = num.toString().split(".");
+    const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return fractionPart
+        ? `${formattedInteger}.${fractionPart}`
+        : formattedInteger;
+}; //addThousandsSeparator(1234567.89)  // "1,234,567.89"
